aeae3eae34ffc1070e1bd3c0335851079f30c3d7
Mayank Maheshwari <mayank.maheshwari625@gmail.com>
2024-05-08 23:09:20 +0530

    added react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import fetchDataFromAPI from './utils/api'
 
 function App() {
   const [count, setCount] = useState(0)
