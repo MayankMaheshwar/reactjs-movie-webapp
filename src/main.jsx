c22e1a1671896752bb6894cf2e4b712ad8a4f50d
Mayank Maheshwari <mayank.maheshwari625@gmail.com>
2024-05-15 23:31:43 +0530

    added redux store in react

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.scss'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
